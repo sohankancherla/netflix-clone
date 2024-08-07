0ed96a4712b657b1c520d82a3cd4cb4b90b0ec48
skancher <sohank@gmail.com>
2024-08-06 23:20:15 -0700

    Initialize prisma

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,4 +1,5 @@
 import { useCallback, useState } from "react";
+import Image from "next/image";
 import Input from "@/components/Input";
 
 const Auth = () => {
@@ -18,7 +19,7 @@ const Auth = () => {
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
         <nav className="px-12 py-5">
-          <img src="/images/logo.png" alt="Logo" className="h-12" />
+          <Image src="/images/logo.png" alt="Logo" className="h-12" />
         </nav>
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 p-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
@@ -58,7 +59,7 @@ const Auth = () => {
             <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
               {variant === "login" ? "Login" : "Sign up"}
             </button>
-            <p className="text-neutral-500 mt-12">
+            <p className="text-neutral-500 mt-12 text-center">
               {variant === "login"
                 ? "First time using Netflix?"
                 : "Already have an account?"}
