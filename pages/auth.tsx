4890038d677c9a9a50f8a44f0c548c238c0136de
skancher <sohank@gmail.com>
2024-08-08 00:44:42 -0700

    Create register api

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -26,7 +26,7 @@ const Auth = () => {
     } catch (error) {
       console.log(error);
     }
-  }, []);
+  }, [email, name, password]);
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -69,7 +69,10 @@ const Auth = () => {
                 value={password}
               />
             </div>
-            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+            <button
+              onClick={register}
+              className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
+            >
               {variant === "login" ? "Login" : "Sign up"}
             </button>
             <p className="text-neutral-500 mt-12 text-center">
