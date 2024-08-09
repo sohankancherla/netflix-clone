dc0d6350283af34604988704eca44d8c2342bb70
skancher <sohank@gmail.com>
2024-08-08 22:46:01 -0700

    create logout button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,31 @@
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+import { redirect } from "next/dist/server/api-utils";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false
+      }
+    };
+  }
+
+  return {
+    props: {}
+  };
+}
+
 export default function Home() {
-  return <h1 className="text-2xl text-green-500">Netflix Clone</h1>;
+  return (
+    <>
+      <h1 className="text-4xl text-green-500">Netflix Clone</h1>
+      <button className="h-10 w-full bg-white" onClick={() => signOut()}>
+        Logout!
+      </button>
+    </>
+  );
 }
