467ab2b67e9cfadf49331f4649498eee6fba8087
skancher <sohank@gmail.com>
2024-08-08 23:10:57 -0700

    Create a profile screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { NextPageContext } from "next";
 import { getSession, signOut } from "next-auth/react";
-import { redirect } from "next/dist/server/api-utils";
+
+import useCurrentUser from "@/hooks/useCurrentUser";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -20,9 +21,12 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
+  const { data: user } = useCurrentUser();
+
   return (
     <>
       <h1 className="text-4xl text-green-500">Netflix Clone</h1>
+      <p className="text-white">Logged in as: {user?.name}</p>
       <button className="h-10 w-full bg-white" onClick={() => signOut()}>
         Logout!
       </button>
